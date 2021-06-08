6c5b0e524ef684e12f780db07982695d53385f2c
Nathan Walker <walkerrunpdx@gmail.com>
2021-06-08 12:46:50 -0700

    chore: Angular 12 updates

--- demo-ng/src/main.ts
+++ demo-ng/src/main.ts
@@ -1,10 +1,6 @@
-import { platformNativeScriptDynamic, platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
+import { platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
 
 import { AppModule } from './app/app.module';
-
-// platformNativeScriptDynamic().bootstrapModule(AppModule);
-
-// platformNativeScript().bootstrapModule(AppModule);
 
 runNativeScriptAngularApp({
 	appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule)
